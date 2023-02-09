cebef120581d83a1c3bda2df133330791b7bb1b2
lgp45 <lgporter@email.neit.edu>
2023-02-09 11:55:40 -0500

    cleanup

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 if(process.env.NODE_ENV === "production")
 {
     module.exports = {mongoURI: "mongodb+srv://lgp45:OXZSa1mwDRkwsULw@cluster0.6tdwqxl.mongodb.net/?retryWrites=true&w=majority"}
+    console.log("Server is running online using Render...");
 }
 else
 {
     module.exports = {mongoURI: "mongodb://localhost:27017/gameEntries"}
+    console.log("Server is running locally...\n")
 }

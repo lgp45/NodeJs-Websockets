c46267ca76c7a7ca59145f3e6897f62d2d2d8020
lgp45 <70779793+lgp45@users.noreply.github.com>
2023-02-20 23:57:16 -0500

    update l2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var mongoose = require("mongoose");
 var port = process.env.port||3000;
 
 var db = require("./config/database");
-
+var dbc = mongoose.connection;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.json());
@@ -71,11 +71,69 @@ app.post("/updateGame", function(req, res){
     });
 });
 
+/*
+app.get("/gameEntries", function(req, res)
+{
+    Game.find({}).sort({game:1}).then(function(game){
+        //console.log({game});
+        res.json({game});
+    });
+    */
+    /*
+    const collection = dbc.collection("games");
+    const sortField = req.query.sortBy || 'title';
+
+    // Extract the sort direction from the query string
+    const sortOrder = req.query.sortOrder === 'desc' ? -1 : 1;
+  
+    // Construct the sort criteria
+    const sort = {};
+    sort[sortField] = sortOrder;
+
+    Game.find().sort(sort).toArray((err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('An error occurred while fetching data from the database');
+        return;
+      }
+
+      res.send(result);
+      dbc.close();
+    });
+    */
+    
+//});
+
 
 app.get("/getID::id", function(req, res){
     console.log(req.body.game._id);
     res.redirect("updatePage.html?id=" + req.params.id);
 })
+
+app.get("/gameEntries", function(req, res) {
+    var { column, sort } = req.query;
+
+    var sortOrder = sort === '-' ? -1 : 1; // if sort is '-', sort in descending order
+    var sortColumn = column || 'game'; // default sort column is 'game'
+    console.log("sorting...");
+    Game.find().sort({ [sortColumn]: sortOrder }).then(function(game) {
+      res.json({ game });
+    }).catch(function(err) {
+      console.error(err);
+      res.status(500).send("An error occurred while fetching data from the database");
+    });
+    
+  });
+
+  app.get('/gameEntries/search', (req, res) => {
+    const searchCol = req.query.searchCol;
+    const searchVal = req.query.searchVal;
+    // handle search logic here
+    console.log("searching...");
+    const games = Game.find({ searchCol: { $regex: game, $options: 'i' } });
+    res.send(games);
+
+  });
 
 
 //Unity Route Testing

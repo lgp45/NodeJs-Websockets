4d3bb2e8dba5f0de2c3a45645175c47e038a1612
lgp45 <70779793+lgp45@users.noreply.github.com>
2023-07-30 01:25:30 -0400

    msg cross

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-
-// const express = require('express');
-// const app = express();
-// const http = require('http').createServer(app);
-// const WebSocket = require('ws');
-// // var port = process.env.port||3000;
-// const wss = new WebSocket.Server({ server: http });
-
-// wss.on('connection', (ws) => {
-//   console.log('A client connected to the WebSocket server.');
-
-//   ws.on('message', (message) => {
-//     console.log('Received message from client:', message);
-//     ws.send("You have joined the socketwerkz.");
-//     // Handle the message from the Unity game server
-//     // and send a response if needed.
-//     // You can implement custom logic here.
-//   });
-
-//   ws.on('close', () => {
-//     console.log('A client disconnected from the WebSocket server.');
-//   });
-// });
-
-// // ... Add your other Express routes and middleware here ...
-
-// const PORT = process.env.PORT || 8080;
-
-// http.listen(PORT, () => {
-//   console.log(`Express server listening on port ${PORT}`);
-// });
-
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
@@ -45,27 +13,39 @@ wss.on('connection', (ws) => {
 
   ws.on('message', (message) => {
     console.log('Received message from client:', message);
-    
+
     // Determine which game server sent the message
-    if (ws === gameServer1) {
+    if (ws === gameServer1) 
+    {
       // Forward the message to gameServer2
-      if (gameServer2 && gameServer2.readyState === WebSocket.OPEN) {
+      if (gameServer2 && gameServer2.readyState === WebSocket.OPEN) 
+      {
         gameServer2.send(message);
       }
-    } else if (ws === gameServer2) {
+    } 
+    else if (ws === gameServer2) 
+    {
       // Forward the message to gameServer1
-      if (gameServer1 && gameServer1.readyState === WebSocket.OPEN) {
+      if (gameServer1 && gameServer1.readyState === WebSocket.OPEN) 
+      {
         gameServer1.send(message);
       }
-    } else {
+    } 
+    else 
+    {
       // This is a new game server connection
-      if (!gameServer1) {
+      if (!gameServer1) 
+      {
         gameServer1 = ws;
         ws.send("You have joined the socketwerkz as Game Server 1.");
-      } else if (!gameServer2) {
+      } 
+      else if (!gameServer2) 
+      {
         gameServer2 = ws;
         ws.send("You have joined the socketwerkz as Game Server 2.");
-      } else {
+      } 
+      else 
+      {
         // Reject additional connections (you can handle this differently if needed)
         ws.send("Connection rejected. Server is full.");
         ws.close();
@@ -75,11 +55,14 @@ wss.on('connection', (ws) => {
 
   ws.on('close', () => {
     console.log('A client disconnected from the WebSocket server.');
-    
+
     // Clean up references when a game server disconnects
-    if (ws === gameServer1) {
+    if (ws === gameServer1) 
+    {
       gameServer1 = null;
-    } else if (ws === gameServer2) {
+    } 
+    else if (ws === gameServer2) 
+    {
       gameServer2 = null;
     }
   });
@@ -87,9 +70,8 @@ wss.on('connection', (ws) => {
 
 // ... Add your other Express routes and middleware here ...
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 http.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
 });
-

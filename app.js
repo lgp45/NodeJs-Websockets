4f858c390288bd0a3d56dfd8ed83c9e359e1dde0
lgp45 <70779793+lgp45@users.noreply.github.com>
2023-07-30 01:37:23 -0400

    port fix

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ wss.on('connection', (ws) => {
 
 // ... Add your other Express routes and middleware here ...
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 http.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);

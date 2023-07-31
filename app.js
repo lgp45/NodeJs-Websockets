740c7432831c767d1d52aa9578d50545e87f9d59
lgp45 <70779793+lgp45@users.noreply.github.com>
2023-07-31 03:10:11 -0400

    cleaning

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const http = require('http').createServer(app);
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ server: http });
 
-// Store references to the connected game servers
+//store references to the connected game servers
 let gameServer1 = null;
 let gameServer2 = null;
 
 wss.on('connection', (ws) => {
-  console.log('A client connected to the WebSocket server.');
+  console.log('A Game-Server connected to the WebSocket server.');
 
   ws.on('message', (message) => {
     console.log('Received message from client:', message);
 
-    // Determine which game server sent the message
+    //determine which game server sent the message
     if (ws === gameServer1) 
     {
-      // Forward the message to gameServer2
+      //forward the message to gameServer2
       if (gameServer2 && gameServer2.readyState === WebSocket.OPEN) 
       {
         gameServer2.send(message);
@@ -25,7 +25,7 @@ wss.on('connection', (ws) => {
     } 
     else if (ws === gameServer2) 
     {
-      // Forward the message to gameServer1
+      //forward the message to gameServer1
       if (gameServer1 && gameServer1.readyState === WebSocket.OPEN) 
       {
         gameServer1.send(message);
@@ -33,20 +33,22 @@ wss.on('connection', (ws) => {
     } 
     else 
     {
-      // This is a new game server connection
+      //this is a new game server connection
       if (!gameServer1) 
       {
         gameServer1 = ws;
-        ws.send("You have joined the socketwerkz as Game Server 1.");
+        Debug.log("Game Server 1 has been cached." + gameServer1.url)
+        ws.send("You have joined the WebSocket Relay as Game Server 1.");
       } 
       else if (!gameServer2) 
       {
         gameServer2 = ws;
-        ws.send("You have joined the socketwerkz as Game Server 2.");
+        Debug.log("Game Server 1 has been cached." + gameServer2.url)
+        ws.send("You have joined the WebSocket Relay as Game Server 2.");
       } 
       else 
       {
-        // Reject additional connections (you can handle this differently if needed)
+        //reject additional connections (you can handle this differently if needed)
         ws.send("Connection rejected. Server is full.");
         ws.close();
       }
@@ -56,7 +58,7 @@ wss.on('connection', (ws) => {
   ws.on('close', () => {
     console.log('A client disconnected from the WebSocket server.');
 
-    // Clean up references when a game server disconnects
+    //clean up references when a game server disconnects
     if (ws === gameServer1) 
     {
       gameServer1 = null;
@@ -68,9 +70,8 @@ wss.on('connection', (ws) => {
   });
 });
 
-// ... Add your other Express routes and middleware here ...
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 http.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
